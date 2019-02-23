f69cf63849e59c05880ca7996aa7cd2c0207a02b
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-23 09:49:15 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,7 +13,10 @@ class School
   
   def add_student(name, grade)
   @roster[grade] = [name]
-   
+   if @roster.has_key?(grade)
+      @roster[grade]<< name
+    else 
+      @roster [grade] = [name]
   end 
 end 
 
