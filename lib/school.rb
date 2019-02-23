e5548343e43091facae69dc65ce9be86664a2474
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-23 10:23:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -41,20 +41,3 @@ end
 
 
 
-# code here!
-# class School
-#   @@roster = {}
-#   def initialize(name)
-#     #school = School.new 
-#     @name = name 
-#     #@@roster.push(name)
-#     end 
-    
-#   def self.roster
-#     @@roster 
-#   end 
-  
-#   def self.add_student(key, value)
-    
-#   end 
-# end 
